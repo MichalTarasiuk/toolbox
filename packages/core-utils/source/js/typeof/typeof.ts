d444136532147e679922dae218b549ebff6ef4d6
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2022-09-15 18:37:16 +0200

    add tests for inferType utility

--- packages/core-utils/source/js/typeof/typeof.ts
+++ packages/core-utils/source/js/typeof/typeof.ts
@@ -1,6 +1,8 @@
+// @TODO: move all types to core-typescript
+
 import { inferType } from './inferType'
 
-// Primitives
+// typeof:primitives
 
 export const isString = (operand: unknown): operand is string =>
   inferType(operand) === 'string'
@@ -9,52 +11,70 @@ export const isNumber = (operand: unknown): operand is number =>
   inferType(operand) === 'number'
 
 export const isBoolean = (operand: unknown): operand is boolean =>
-  inferType(operand) === 'number'
+  inferType(operand) === 'boolean'
 
-export const isSymbol = (operand: unknown): operand is Symbol =>
+export const isSymbol = (operand: unknown): operand is symbol =>
   inferType(operand) === 'symbol'
 
-export const isBigInt = (operand: unknown): operand is BigInt =>
-  inferType(operand) === 'bigInt'
+export const isBigInt = (operand: unknown): operand is bigint =>
+  inferType(operand) === 'bigint'
 
-// Functions
+export const isUndefined = (operand: unknown) =>
+  inferType(operand) === 'undefined'
+
+export const isNull = (operand: unknown) => inferType(operand) === 'null'
+
+// typeof:functions
 
 export const isFunction = (operand: unknown) =>
   inferType(operand) === 'function'
 
-export const isGeneratorFunction = (operand: unknown) =>
-  inferType(operand) === 'generatorFunction'
-
-export const isAsyncFunction = (operand: unknown) =>
-  inferType(operand) === 'asyncFunction'
-
-// Objects
-
-export const isObject = (
+type AnyGeneratorFunction = (...args: any[]) => Generator<unknown, any, unknown>
+export const isGeneratorFunction = (
   operand: unknown,
-): operand is Record<PropertyKey, string> => inferType(operand) === 'object'
+): operand is AnyGeneratorFunction =>
+  inferType(operand, true) === 'generatorfunction'
 
-export const isArray = (operand: unknown): operand is Array<unknown> =>
-  inferType(operand) === 'array'
+type AnyAsyncFunction = (...args: any[]) => Promise<unknown>
+export const isAsyncFunction = (
+  operand: unknown,
+): operand is AnyAsyncFunction => inferType(operand, true) === 'asyncfunction'
 
-export const isRegExp = (operand: unknown) => inferType(operand) === 'regExp'
+// typeof:objects
 
-export const isPromise = (operand: unknown) => inferType(operand) === 'promise'
+type AnyObject = Record<PropertyKey, unknown>
+export const isObject = (operand: unknown): operand is AnyObject =>
+  inferType(operand, true) === 'object'
 
-// Classes
+type AnyArray = Array<unknown>
+export const isArray = (operand: unknown): operand is AnyArray =>
+  inferType(operand, true) === 'array'
 
-export const isMap = (operand: unknown) => inferType(operand) === 'map'
+export const isRegExp = (operand: unknown): operand is RegExp =>
+  inferType(operand, true) === 'regexp'
 
-export const isSet = (operand: unknown) => inferType(operand) === 'set'
+type AnyPromise = Promise<unknown>
+export const isPromise = (operand: unknown): operand is AnyPromise =>
+  inferType(operand, true) === 'promise'
 
-export const isWeakMap = (operand: unknown) => inferType(operand) === 'weakMap'
+type AnyMap = Map<unknown, unknown>
+export const isMap = (operand: unknown): operand is AnyMap =>
+  inferType(operand, true) === 'map'
 
-export const isWeakSet = (operand: unknown) => inferType(operand) === 'weakSet'
+type AnySet = Set<unknown>
+export const isSet = (operand: unknown): operand is AnySet =>
+  inferType(operand, true) === 'set'
 
-export const isDate = (operand: unknown) => inferType(operand) === 'date'
+type AnyWeakMap = WeakMap<AnyObject, unknown>
+export const isWeakMap = (operand: unknown): operand is AnyWeakMap =>
+  inferType(operand, true) === 'weakmap'
 
-export const isProxy = (operand: unknown) => inferType(operand) === 'proxy'
+type AnyWeakSet = WeakSet<AnyObject>
+export const isWeakSet = (operand: unknown): operand is AnyWeakSet =>
+  inferType(operand, true) === 'weakset'
 
-export const isError = (operand: unknown) => inferType(operand) === 'error'
+export const isDate = (operand: unknown): operand is Date =>
+  inferType(operand, true) === 'date'
 
-export const isMath = (operand: unknown) => inferType(operand) === 'math'
+export const isError = (operand: unknown): operand is Error =>
+  inferType(operand, true) === 'error'
