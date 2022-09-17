d538304b57585b4b34f9766e1908528f462f65f0
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2022-09-17 19:02:13 +0200

    add tests for js:custom:higherOrder

--- packages/core-utils/tests/js/custom.test.ts
+++ packages/core-utils/tests/js/custom.test.ts
@@ -1,7 +1,12 @@
-import { createAutoPercentage } from '../../source/js/custom/custom'
+import {
+  createAutoPercentage,
+  not,
+  thunkify,
+} from '../../source/js/custom/custom'
 
 describe('js:custom', () => {
-  it('js:custom:factories:createAutoPercentage', () => {
+  it('js:custom:factories', () => {
+    // js:custom:factories:createAutoPercentage
     const autoPercentage = createAutoPercentage()
     const steps = [
       { name: 'init', progress: autoPercentage.percentage() },
@@ -16,5 +21,16 @@ describe('js:custom', () => {
     })
 
     expect(progess).toBe(1)
+  })
+
+  it('js:custom:higherOrder', () => {
+    // js:custom:higherOrder:not
+    expect([0, true, [], {}, ''].filter(not(Boolean))).toEqual([0, ''])
+
+    // js:custom:higherOrder:thunkify
+    const thunk = thunkify((a: number, b: number) => a + b)
+
+    expect(thunk(1, 2)).toBe(3)
+    expect(thunk(3, 4))
   })
 })
