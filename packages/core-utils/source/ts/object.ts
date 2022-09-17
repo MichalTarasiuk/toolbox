e8c3950fb66010fc18e05e91c12211b2f1da1148
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2022-09-17 23:26:52 +0200

    update ts:object utils

--- packages/core-utils/source/ts/object.ts
+++ packages/core-utils/source/ts/object.ts
@@ -1,9 +1,25 @@
 /**
- * Returns the names of the enumerable string properties and methods of an object.
- *
  * @param object - Object that contains the properties and methods.
  *
- * @returns properties and methods of object
+ * @returns the names of the enumerable string properties and methods of an object
  */
 export const objectKeys = <Object extends AnyObject>(object: Object) =>
   Object.keys(object) as Array<keyof Object>
+
+/**
+ * @param object - Object that contains the properties and methods.
+ *
+ * @returns an array of key/values of the enumerable properties of an object
+ */
+export const entries = <Object extends AnyObject>(object: Object) =>
+  Object.entries(object) as Array<[keyof Object, Object[keyof Object]]>
+
+/**
+ * @param entries - An iterable object that contains key-value entries for properties and methods.
+ *
+ * @returns an object created by key-value entries for properties and methods
+ */
+export const fromEntries = <Entries extends Array<[PropertyKey, unknown]>>(
+  entries: Entries,
+) =>
+  Object.fromEntries(entries) as Record<Entries[number][0], Entries[number][1]>
