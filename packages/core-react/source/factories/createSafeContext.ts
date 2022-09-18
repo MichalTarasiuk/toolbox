b82154d33770719739c67061e3e7c6aedbda647d
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2022-09-18 13:52:37 +0200

    add createSafeContext for react

---
+++ packages/core-react/source/factories/createSafeContext.ts
@@ -0,0 +1,46 @@
+import {
+  createContext as createContextImpl,
+  useContextSelector,
+} from 'use-context-selector'
+
+import { uppercaseFirst } from '../../../core-utils/source/source'
+import type { Context } from 'react'
+
+type Selector<TContextValue, TSelected> = (
+  contextValue: TContextValue,
+) => TSelected
+
+const defaultContextValue = Symbol()
+
+const getErrorMessage = (name: string) =>
+  `use${name} must be used within a ${name}Provider`
+
+const createContext = <TContextValue>() =>
+  createContextImpl(defaultContextValue) as unknown as Context<TContextValue>
+
+const isInvalidHookCall = (contextValue: unknown) =>
+  contextValue === defaultContextValue
+
+export const createSafeContext = <TContextValue>(name: string) => {
+  const safeContext = createContext<TContextValue>()
+  const uppercasedName = uppercaseFirst(name)
+
+  const useSafeContext = <TSelected = TContextValue>(
+    selector?: Selector<TContextValue, TSelected>,
+  ) => {
+    const identity = (value: TContextValue) => value as unknown as TSelected
+    const selected = useContextSelector(safeContext, selector ?? identity)
+
+    if (isInvalidHookCall(selected)) {
+      const errorMessage = getErrorMessage(name)
+
+      throw Error(errorMessage)
+    }
+
+    return selected
+  }
+
+  safeContext.displayName = `${uppercasedName}Context`
+
+  return [safeContext.Provider, useSafeContext] as const
+}
