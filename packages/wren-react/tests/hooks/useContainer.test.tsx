2b4e725ea74f557f3433360a6b0bc3e7a3c49901
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-01 20:34:31 +0200

    test all hook related to useFiber hook

---
+++ packages/wren-react/tests/hooks/useContainer.test.tsx
@@ -0,0 +1,19 @@
+import { renderHook } from '@testing-library/react'
+import React from 'react'
+
+import { useContainer } from '../../source/hooks/hooks'
+import { FiberProvider } from '../../source/providers/providers'
+
+import type { ReactNode } from 'react'
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+	<FiberProvider>{children}</FiberProvider>
+)
+
+describe('react:hooks:useContainer', () => {
+	it('should return container', () => {
+		const { result } = renderHook(() => useContainer(), { wrapper })
+
+		expect(result).toBeDefined()
+	})
+})
---
+++ packages/wren-react/tests/hooks/useNearestParent.test.tsx
@@ -0,0 +1,37 @@
+import { renderHook } from '@testing-library/react'
+import React from 'react'
+
+import { useNearestParent } from '../../source/hooks/hooks'
+import { FiberProvider } from '../../source/providers/providers'
+
+import type { ReactNode } from 'react'
+
+describe('react:hooks:useNearestChild', () => {
+	it('should return nearest parent', () => {
+		const wrapper = ({ children }: { children: ReactNode }) => (
+			<FiberProvider>
+				<div>
+					<p>some content</p>
+					{children}
+				</div>
+			</FiberProvider>
+		)
+		const { result } = renderHook(() => useNearestParent(), {
+			wrapper,
+		})
+
+		expect(result).toBeDefined()
+	})
+
+	it('should return `undefined` when the parent was not found', () => {
+		const wrapper = ({ children }: { children: ReactNode }) => (
+			<FiberProvider>{children}</FiberProvider>
+		)
+
+		const { result } = renderHook(() => useNearestParent(), {
+			wrapper,
+		})
+
+		expect(result.current).toEqual({ current: undefined })
+	})
+})
