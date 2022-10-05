c161aca40d7658ac62f4fd6876f427e4ef3ce48f
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-05 17:59:29 +0200

    format configs files

--- jest.config.ts
+++ jest.config.ts
@@ -5,21 +5,21 @@ import type { Config } from 'jest'
 
 const basePath = path.resolve(__dirname, 'packages')
 const packages = readdirSync(basePath)
-	.filter((name) => lstatSync(path.join(basePath, name)).isDirectory())
-	.sort((a, b) => b.length - a.length)
+  .filter((name) => lstatSync(path.join(basePath, name)).isDirectory())
+  .sort((a, b) => b.length - a.length)
 
 const projects = packages.map((displayName) => ({
-	displayName,
-	testEnvironment: 'jsdom',
-	testMatch: [`<rootDir>/packages/${displayName}/**/*.test.(ts|tsx)`],
-	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  displayName,
+  testEnvironment: 'jsdom',
+  testMatch: [`<rootDir>/packages/${displayName}/**/*.test.(ts|tsx)`],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }))
 
 const jestConfig: Config = {
-	verbose: true,
-	errorOnDeprecated: true,
-	clearMocks: true,
-	projects,
+  verbose: true,
+  errorOnDeprecated: true,
+  clearMocks: true,
+  projects,
 }
 
 export default jestConfig
--- jest.setup.ts
+++ jest.setup.ts
@@ -3,10 +3,10 @@ import * as util from 'util'
 // ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 // ref: https://github.com/jsdom/jsdom/issues/2524
 Object.defineProperty(global, 'TextEncoder', {
-	writable: true,
-	value: util.TextEncoder,
+  writable: true,
+  value: util.TextEncoder,
 })
 Object.defineProperty(global, 'TextDecoder', {
-	writable: true,
-	value: util.TextDecoder,
+  writable: true,
+  value: util.TextDecoder,
 })
