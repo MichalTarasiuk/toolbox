1bb76d40d5a449af110b2191bde1ae27642702ff
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-06 21:33:16 +0200

    add addPackage script

--- scripts/addPackage.ts
+++ scripts/addPackage.ts
@@ -1,17 +1,56 @@
-import path from 'path'
-
-import { signs } from '@bulb/utils'
-import makeDir from 'make-dir'
+import { isString, signs, timeout } from '@bulb/utils'
+import prompt from 'prompt'
 
 import packageJSON from '../package.json'
 
-const repositoryName: string = packageJSON.name
-const workspace = path.join
+import { createTree, getWorkspace } from './utils'
 
+const repositoryName: string = packageJSON.name
 const addPrefix = (name: string) => `${repositoryName}${signs.minus}${name}`
 
-export const addPackage = (name: string) => {
-  const packageName = addPrefix(name)
+const getName = () => {
+  const value = 'name'
 
-  makeDir()
+  return new Promise<string>((resolve, reject) => {
+    prompt.start()
+
+    prompt.get([value], (error, properties) => {
+      if (error) {
+        reject(error)
+      }
+
+      const name = properties[value]
+
+      if (isString(name)) {
+        resolve(name)
+      }
+    })
+  })
 }
+
+export const addPackage = async () => {
+  try {
+    const namePromise = getName()
+    const name = await timeout(namePromise, 4_000)
+
+    const packageName = addPrefix(name)
+    const workspace = getWorkspace('packages')
+
+    const tree = createTree(workspace)
+
+    await tree.render({
+      name: packageName,
+      children: [
+        { name: 'source', children: ['source.ts'] },
+        'test',
+        '_api.ts',
+        'tsconfig.json',
+        'package.json',
+      ],
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+void addPackage()
---
+++ scripts/utils.ts
@@ -0,0 +1,48 @@
+import { join } from 'path'
+
+import { isArray, isObject, isString, keyIn } from '@bulb/utils'
+import createFile from 'create-file'
+import makeDir from 'make-dir'
+
+export const getWorkspace = (name: string = '') => join(process.cwd(), name)
+export const hasExtension = (name: string) => /^\w+.\w+$/.test(name)
+
+// scripts:utils:createTree
+
+type Tree = {
+  name: string
+  children: Array<Tree | string>
+}
+
+type TreeNode = Tree | string
+
+const isTree = (value: unknown): value is Tree => {
+  const canCheck = isObject(value)
+
+  if (canCheck) {
+    const hasName = keyIn(value, 'name') && isString(value.name)
+    const hasChildren = keyIn(value, 'children') && isArray(value.children)
+
+    return hasName && hasChildren
+  }
+
+  return canCheck
+}
+
+export const createTree = (path: string) => {
+  const render = async (treeNode: TreeNode) => {
+    if (isTree(treeNode)) {
+      await makeDir(join(path, treeNode.name))
+
+      await Promise.all(treeNode.children.map((child) => render(child)))
+
+      return
+    }
+
+    await (hasExtension(treeNode) ? createFile(treeNode) : makeDir(treeNode))
+  }
+
+  return {
+    render,
+  }
+}
