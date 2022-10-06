4e4ed2e88d8ececa26ddecdbc6398004f1993abe
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-06 22:32:47 +0200

    fix typo

--- packages/bulb-react/tests/hooks/useLayout.node-test.ts
+++ packages/bulb-react/tests/hooks/useLayout.node-test.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 
 import { useLayout } from '../../source/hooks/useLayout'
 
-describe('jsdom - react:hooks:useLayout', () => {
+describe('node - react:hooks:useLayout', () => {
   it('should return `useEffect` on server', () => {
     const {
       result: { current: hook },
