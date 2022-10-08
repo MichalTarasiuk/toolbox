3db27b400ba5604b1b847192b89e98242404afba
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-08 14:16:26 +0200

    add useCustomCompareMemo hook

---
+++ packages/bulb-react/source/hooks/useCustomCompareMemo.ts
@@ -0,0 +1,34 @@
+import { useMemo, useRef } from 'react'
+
+import type { DependencyList } from 'react'
+
+/**
+ * @Deprecated - moved to `@react-hookz/web`: https://github.com/react-hookz/web/pull/895
+ *
+ * Like useMemo but uses provided comparator function to validate dependency changes.
+ *
+ * @param factory useMemo factory function
+ * @param deps useMemo dependency list
+ * @param comparator function to validate dependency changes
+ *
+ * @returns useMemo result
+ */
+export const useCustomCompareMemo = <
+  Factory extends () => unknown,
+  Deps extends DependencyList,
+>(
+  factory: Factory,
+  deps: Deps,
+  comparator: (a: Deps, b: Deps) => boolean,
+) => {
+  const dependencies = useRef<Deps>()
+
+  if (
+    dependencies.current === undefined ||
+    !comparator(dependencies.current, deps)
+  ) {
+    dependencies.current = deps
+  }
+
+  return useMemo(factory, dependencies.current)
+}
