3ef6d7fa8de0b713156a40860be0552e9d20471e
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-19 18:49:43 +0200

    create 'createSafeContext' factory

--- packages/brainless-react/source/factories/factories.ts
+++ packages/brainless-react/source/factories/factories.ts
@@ -1,3 +1,4 @@
 export * from './createSelectorContext'
 export * from './createStepperContext/createStepperContext'
 export * from './createAtoms/createAtoms'
+export * from './createSafeContext'
--- packages/brainless-react/tests/factories/createSafeContext.jsdom-test.tsx
+++ packages/brainless-react/tests/factories/createSafeContext.jsdom-test.tsx
@@ -1,114 +1,46 @@
-import { noop } from '@brainless/utils'
-import { fireEvent, render } from '@testing-library/react'
-import React, { useState } from 'react'
+import { render } from '@testing-library/react'
+import mockConsole from 'jest-mock-console'
+import React from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
-import { createSelectorContext } from '../../_api'
+import { createSafeContext } from '../../_api'
 
-import type { Any } from '@brainless/typescript'
 import type { ReactNode } from 'react'
 
-type ContextValue = {
-  name: string
-  age: number
-}
+describe('react:factories:createSafeContext', () => {
+  it('should render', () => {
+    const [UserProvider, useUser] = createSafeContext<{ name: string }>('user')
 
-type RenderProps = {
-  withProvider?: boolean
-  displayNameSpy?: Any.UnknownFunction
-}
-type SelectorContextProviderProps = RenderProps & {
-  children: ReactNode
-}
-type DisplayNameProps = Required<Pick<RenderProps, 'displayNameSpy'>>
+    const Component = () => {
+      const user = useUser()
 
-const createTree = (initialContextValue: ContextValue) => {
-  const [SelectorContextProvider, useSelectorContext] =
-    createSelectorContext<ContextValue>('tree')
-
-  const FallbackComponent = () => <p>ERROR: something went wrong ¯\_(ツ)_/¯</p>
-
-  const DisplayName = (props: DisplayNameProps) => {
-    const name = useSelectorContext((value) => value.name)
-
-    props.displayNameSpy()
-
-    return <p>{`name: ${name}`}</p>
-  }
-
-  const DisplayAge = () => {
-    const age = useSelectorContext((value) => value.age)
-
-    return <p>{`age: ${age}`}</p>
-  }
-
-  const OptionalSelectorContextProvider = (
-    props: SelectorContextProviderProps,
-  ) => {
-    const [contextValue, setContextValue] = useState(initialContextValue)
-
-    const simulateYear = () => {
-      setContextValue({ ...contextValue, age: contextValue.age + 1 })
+      return <p>name {user.name}</p>
     }
-
-    if (props.withProvider) {
-      return (
-        <SelectorContextProvider value={contextValue}>
-          {props.children}
-          <button onClick={simulateYear}>simulate year</button>
-        </SelectorContextProvider>
-      )
-    }
-
-    return <>{props.children}</>
-  }
-
-  const Tree = (props?: RenderProps) => {
-    const { withProvider = true } = props || {}
-
-    return (
-      <ErrorBoundary FallbackComponent={FallbackComponent}>
-        <OptionalSelectorContextProvider withProvider={withProvider}>
-          <DisplayName displayNameSpy={props?.displayNameSpy || noop} />
-          <DisplayAge />
-        </OptionalSelectorContextProvider>
-      </ErrorBoundary>
+    const Wrapper = ({ children }: { children: ReactNode }) => (
+      <UserProvider value={{ name: 'Michał' }}>{children}</UserProvider>
     )
-  }
 
-  return Tree
-}
-
-describe('react:factories:createSelectorContext', () => {
-  it('should render correctly', () => {
-    const Tree = createTree({ name: 'Michał', age: 19 })
-
-    const { getByText } = render(<Tree />)
-
-    getByText('simulate year')
+    render(<Component />, { wrapper: Wrapper })
   })
 
-  it('should rerender `DisplayAge` component', async () => {
-    const initialContextValue = { name: 'Michał', age: 19 }
-    const Tree = createTree(initialContextValue)
+  it('should not render', () => {
+    const restoreConsole = mockConsole()
+    const [_, useUser] = createSafeContext<{ name: string }>('user')
 
-    const { findByText, getByText } = render(<Tree />)
+    const Component = () => {
+      const user = useUser()
 
-    fireEvent.click(getByText('simulate year'))
+      return <p>name {user.name}</p>
+    }
+    const { getByText } = render(
+      <ErrorBoundary fallback={<p>fallback</p>}>
+        <Component />
+      </ErrorBoundary>,
+    )
 
-    await findByText(`age: ${initialContextValue.age + 1}`)
-  })
+    expect(console.error).toHaveBeenCalled()
+    getByText('fallback')
 
-  it('should not rerender `DisplayName` component on update age', () => {
-    const initialContextValue = { name: 'Michał', age: 19 }
-    const displayNameSpy = jest.fn()
-    const Tree = createTree(initialContextValue)
-
-    const { getByText } = render(<Tree displayNameSpy={displayNameSpy} />)
-
-    fireEvent.click(getByText('simulate year'))
-
-    getByText(`age: ${initialContextValue.age + 1}`)
-    expect(displayNameSpy).toHaveBeenCalledTimes(3)
+    restoreConsole()
   })
 })
