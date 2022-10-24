0ab99f580e7a364757778d9c79cb55b241be0d33
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-10-24 19:26:38 +0200

    feat: add settings prop for contextify

--- packages/jupiter-react/source/utils/contextify.tsx
+++ packages/jupiter-react/source/utils/contextify.tsx
@@ -1,5 +1,3 @@
-// @TODO: add support for params of hook
-
 import { none } from '@jupiter/utils'
 import React from 'react'
 
@@ -8,19 +6,31 @@ import { createSafeContext } from '../source'
 import type { Any } from '@jupiter/typescript'
 import type { ReactNode } from 'react'
 
-export const contextify = <UseHook extends Any.UnknownFunction>(
+type InferSettings<Params extends Any.AnyArray> = {
+  [Key in Extract<keyof Params, `${number}`>]: Params[Key]
+}
+
+type ContextProvider<Params extends Any.AnyArray> = {
+  children: ReactNode
+  settings: InferSettings<Params>
+}
+
+export const contextify = <UseHook extends Any.AnyFunction>(
   useHook: UseHook,
 ) => {
   const name = useHook.name.replace('use', none)
-  const [SafeProviderImpl, useSafeContext] =
+  const [ContextProviderImpl, useContext] =
     createSafeContext<ReturnType<UseHook>>(name)
 
-  const SafeProvider = ({ children }: { children: ReactNode }) => {
+  const ContextProvider = ({
+    children,
+    settings,
+  }: ContextProvider<Parameters<UseHook>>) => {
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- fix later
-    const hook = useHook() as ReturnType<UseHook>
+    const hook = useHook(...Object.values(settings)) as ReturnType<UseHook>
 
-    return <SafeProviderImpl value={hook}>{children}</SafeProviderImpl>
+    return <ContextProviderImpl value={hook}>{children}</ContextProviderImpl>
   }
 
-  return [SafeProvider, useSafeContext] as const
+  return [ContextProvider, useContext] as const
 }
