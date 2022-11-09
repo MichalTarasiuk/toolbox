45f67fa5bd7f3acf730ef4ef71f0ef1a7c2c1d23
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-11-09 19:52:10 +0100

    feat: clean code in atomify (remove duplicated code)

--- packages/wren-react/source/factories/atomify/helpers/helpers.ts
+++ packages/wren-react/source/factories/atomify/helpers/helpers.ts
@@ -1,51 +1,8 @@
-import { isFunction } from '@wren/utils'
-import equal from 'deep-equal'
 import { v4 } from 'uuid'
 
-import type {
-  Get,
-  Initialization,
-  LazyInitialization,
-  ResolvableState,
-} from '../types'
-import type { Any } from '@wren/typescript'
-
-export { collectExtensions } from './collectExtensions'
-
-export const createState = <State>() => {
-  const initialState = Symbol()
-  let state: State | typeof initialState = initialState
-
-  const canUpdate = <State>(state: State, nextState: State) =>
-    !equal(state, nextState)
-
-  const update = (nextState: State) => {
-    if (state === initialState || canUpdate(state, nextState)) {
-      state = nextState
-    }
-
-    return state
-  }
-
-  return {
-    update,
-  }
-}
-
-const isResolveableState = <State>(
-  resolvableState: ResolvableState<State>,
-): resolvableState is (state: State) => State => isFunction(resolvableState)
-
-export const resolveState = <State>(
-  resolvableState: ResolvableState<State>,
-  state: State,
-) => {
-  if (isResolveableState(resolvableState)) {
-    return resolvableState(state)
-  }
-
-  return resolvableState
-}
+export * from './state'
+export * from './initialization'
+export * from './collectExtensions'
 
 export const createWorker = () => {
   const id = v4()
@@ -65,26 +22,4 @@ export const createWorker = () => {
     stop,
     id,
   }
-}
-
-const canResolvInitialization = <State>(
-  initialization: Initialization<State>,
-): initialization is LazyInitialization<State> => isFunction(initialization)
-
-export const initialize = <State>(
-  initialization: Initialization<State>,
-  get: Get,
-  lifecycle: { before: Any.Noop },
-) => {
-  lifecycle.before()
-
-  if (canResolvInitialization(initialization)) {
-    const state = initialization(get)
-
-    initialization.get?.(state)
-
-    return state
-  }
-
-  return initialization
 }
