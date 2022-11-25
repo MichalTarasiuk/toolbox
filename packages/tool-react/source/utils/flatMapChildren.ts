70f2eb57445175c1cb42d62a7aeab2846e677fd1
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-11-25 23:09:57 +0100

    feat: add flatMapChildren utility

---
+++ packages/tool-react/source/utils/flatMapChildren.ts
@@ -0,0 +1,33 @@
+import {isNumber, isObject, isString, keyIn, signs} from '@tool/utils';
+import {Children, cloneElement} from 'react';
+import ReactIs from 'react-is';
+
+import type {ReactNode, Key, ReactElement} from 'react';
+
+const hasChildren = (props: unknown): props is {children: ReactNode} => isObject(props) && keyIn(props, 'children');
+const isTag = (reactNode: ReactNode): reactNode is ReactElement =>
+  ReactIs.isElement(reactNode) && !ReactIs.isFragment(reactNode);
+
+export const flatMapChildren = (children: ReactNode, keys: Array<Key> = []) => {
+  return Children.toArray(children).reduce<ReactNode[]>((collector, reactNode, index) => {
+    if (ReactIs.isFragment(reactNode) && hasChildren(reactNode.props)) {
+      collector.push(...flatMapChildren(reactNode.props.children, keys.concat(reactNode.key ?? index)));
+    }
+
+    if (isTag(reactNode)) {
+      const key = keys.join(signs.dot) + String(reactNode.key);
+
+      collector.push(
+        cloneElement(reactNode, {
+          key,
+        }),
+      );
+    }
+
+    if (isString(reactNode) || isNumber(reactNode)) {
+      collector.push(reactNode);
+    }
+
+    return collector;
+  }, []);
+};
---
+++ packages/tool-react/tests/utils/flatMapChildren.jsdom-test.tsx
@@ -0,0 +1,56 @@
+import {render} from '@testing-library/react';
+import React from 'react';
+import ReactIs from 'react-is';
+
+import {flatMapChildren} from '../../_api';
+
+import type {ReactNode} from 'react';
+
+describe('jsdom - react:utils:flatMapChildren', () => {
+  it('should filter all elements which are not a `Item` component', () => {
+    const Item = ({children}: {children: string}) => <p>{children}</p>;
+    const List = ({children}: {children: ReactNode}) => (
+      <>{flatMapChildren(children).filter(reactNode => ReactIs.isElement(reactNode) && reactNode.type === Item)}</>
+    );
+
+    const {getByText} = render(
+      <List>
+        <Item>1</Item>
+        <Item>2</Item>
+        <>
+          <p>3</p>
+          <Item>4</Item>
+        </>
+      </List>,
+    );
+
+    expect(() => getByText('3')).toThrowError();
+  });
+
+  it('should skip fragment tag', () => {
+    const FlatMapList = ({children}: {children: ReactNode}) => <p>flat map: {flatMapChildren(children).length}</p>;
+    const MapList = ({children}: {children: ReactNode}) => <p>map: {React.Children.toArray(children).length}</p>;
+
+    const {getByText} = render(
+      <>
+        <FlatMapList>
+          <p>1</p>
+          <>
+            <p>2</p>
+            <p>3</p>
+          </>
+        </FlatMapList>
+        <MapList>
+          <p>1</p>
+          <>
+            <p>2</p>
+            <p>3</p>
+          </>
+        </MapList>
+      </>,
+    );
+
+    getByText('flat map: 3');
+    getByText('map: 2');
+  });
+});
