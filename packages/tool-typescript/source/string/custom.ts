ec9c46f377d5de2c68a0040a4eb59c0fc71c5272
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-11-30 21:49:22 +0100

    feat: initialize custom file in tool-typescript/string

--- packages/tool-typescript/source/string/custom.ts
+++ packages/tool-typescript/source/string/custom.ts
@@ -1,1 +1,8 @@
-export type Mock = null;
+/**
+ * Drop a specified char from a string.
+ */
+export type DropChar<Word extends string, Char extends string> = Word extends `${infer First}${infer Rest}`
+  ? First extends Char
+    ? DropChar<Rest, Char>
+    : `${First}${DropChar<Rest, Char>}`
+  : Word;
