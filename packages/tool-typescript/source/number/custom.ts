9c2cccceda36299cca061418905f479c9dc9e1eb
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2022-12-13 18:28:35 +0100

    feat: addOne custom generic type for number

--- packages/tool-typescript/source/number/custom.ts
+++ packages/tool-typescript/source/number/custom.ts
@@ -1,1 +1,5 @@
-export type Mock = null;
+import type {Array} from '../../_api';
+
+export type AddOne<Digit extends number, Arr = Array.From<Digit>> = Arr extends never[]
+  ? Array.Length<[...Arr, never]>
+  : never;
