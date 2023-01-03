ef8364d22bbb2ea4a661a66db60c3d481ef5eeeb
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2023-01-03 08:27:29 +0100

    fix: performence of useMemoCache hook

--- packages/tool-react/source/hooks/useMemoCache/cache.ts
+++ packages/tool-react/source/hooks/useMemoCache/cache.ts
@@ -3,26 +3,44 @@ import {areHookInputsEqual} from '../../../_api';
 import type {DependencyList} from 'react';
 
 type None = typeof none;
+type CachedItem<State> = {state: State; dependencyList: DependencyList};
 
 const none = Symbol();
 
 export const createCache = <State>() => {
-  const cache: Array<[DependencyList, State]> = [];
+  const cache = new Map<string, Array<CachedItem<State>>>();
 
   const get = (dependencyList: DependencyList) => {
-    const cacheEntry = cache.find(([cachedDependencyList]) => areHookInputsEqual(cachedDependencyList, dependencyList));
+    const key = String(dependencyList);
+    const cached = cache.get(key);
 
-    if (!cacheEntry) {
+    if (!cached) {
       return none;
     }
 
-    const cachedState = cacheEntry[1];
+    const cachedItem = cached.find(item => areHookInputsEqual(item.dependencyList, dependencyList));
 
-    return cachedState;
+    if (cachedItem) {
+      return cachedItem.state;
+    }
+
+    return none;
   };
 
   const set = (dependencyList: DependencyList, state: State) => {
-    cache.push([dependencyList, state]);
+    const key = String(dependencyList);
+
+    const hasCachedItem = cache.has(key);
+
+    if (!hasCachedItem) {
+      cache.set(key, []);
+    }
+
+    const cachedItem = cache.get(key);
+
+    if (cachedItem) {
+      cachedItem.push({dependencyList, state});
+    }
   };
 
   const isNone = (state: None | State): state is None => state === none;
